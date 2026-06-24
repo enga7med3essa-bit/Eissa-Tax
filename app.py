952715e911fe6ab4ddee865c767bdb1a5efdb956
enga7med3essa-bit <eng.a7med3essa@gmail.com>
2026-06-24 14:13:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-def calculate_tax(income):
-    if income <= 60000:
-        return 0
-    elif income <= 200000:
-        return (income - 60000) * 0.10
-    elif income <= 400000:
-        return 14000 + (income - 200000) * 0.15
-    else:
-        return 44000 + (income - 400000) * 0.20
+import streamlit as st
+
+st.title("Eissa Tax App")
+st.write("لو شايف الرسالة دي يبقى التطبيق شغال 👍")
